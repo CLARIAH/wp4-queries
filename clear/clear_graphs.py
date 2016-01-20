2fc0b2a521e35cc4e97ec8156cd9c1d8195227c2
kathrinrin <k.dentler@vu.nl>
2016-01-20 16:14:53 +0100

    fixed SPARQLWrapper POST warning

--- clear/clear_graphs.py
+++ clear/clear_graphs.py
@@ -1,34 +1,36 @@
-import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, POST, JSON
 import re
 import time 
 
-endpoint = SPARQLWrapper.SPARQLWrapper("http://virtuoso.clariah-sdh.eculture.labs.vu.nl/sparql")
+endpoint = SPARQLWrapper("http://virtuoso.clariah-sdh.eculture.labs.vu.nl/sparql")
+
 
 # w/o count query is very slow
 endpoint.setQuery("""
-    SELECT ?graph COUNT(*) AS ?triples 
+    SELECT DISTINCT ?graph COUNT(*) AS ?triples 
     WHERE { 
         GRAPH ?graph {?s ?p ?o}
     }
-    ORDER BY DESC(?triples)
+    ORDER BY ?graph
 """)
-endpoint.setReturnFormat(SPARQLWrapper.JSON)
+
+endpoint.setReturnFormat(JSON)
 results = endpoint.query().convert()
 
-graphs_01_19 = []
+graphs = []
 for result in results['results']["bindings"]:
-    x = result['graph']['value']
-    if re.search("01-19", x): # could also filter in query, but seems very slow
-        graphs_01_19.append(x)
+    graph = result['graph']['value']
+    if re.search("2016-01-20", graph): # could also filter in query, but seems very slow
+        graphs.append(graph)
 
-# test on one graph
 # querystring = "CLEAR GRAPH <%s>" % graphs_01_19[0]
 # endpoint.setQuery(querystring)
 # endpoint.query()
 
-for graph in graphs_01_19:
+for graph in graphs:
     print graph 
     querystring = "CLEAR GRAPH <%s>" % graph
     endpoint.setQuery(querystring)
+    endpoint.setMethod(POST)
     endpoint.query()
     time.sleep(5) 
