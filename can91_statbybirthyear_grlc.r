cbcac9b54322e9328ebb44e59f6521ca845a2676
rlzijdeman <richard.zijdeman@iisg.nl>
2016-05-28 23:25:24 +0200

    added alternative model with interaction (remove comments to run)

--- can91_statbybirthyear_grlc.r
+++ can91_statbybirthyear_grlc.r
@@ -9,11 +9,24 @@ sweden <- getURL("http://grlc.clariah-sdh.eculture.labs.vu.nl/clariah/wp4-querie
 sweden <- read.csv(textConnection(sweden))
 
 fit_canada_base <- lm(log(hiscam) ~ log(gdppc), data=canada)
+
 fit_canada <- lm(log(hiscam) ~ log(gdppc) + I(age^2) + age, data=canada)
+#summary(fit_canada)
+#fit_canada2 <- update(fit_canada, . ~ . + I(age*log(gdppc)))
+#summary(fit_canada2)
+
+
+
+
 fit_sweden_base <- lm(log(hiscam) ~ log(gdppc), data=sweden)
 fit_sweden <- lm(log(hiscam) ~ log(gdppc) + I(age^2) + age, data=sweden)
+#fit_sweden2 <- update(fit_sweden, . ~ . + I(age*log(gdppc)))
+#summary(fit_sweden2)
+
+
 
 texreg::texreg(list(fit_canada, fit_sweden))
+#texreg::texreg(list(fit_canada2, fit_sweden2))
 
 pdf('~/dropbox/files attached directly to project/presentations/usecaseplots.pdf', height=3, width=10)
 par(mfrow=c(1, 4), mar=c(4, 4, 3, 0))
