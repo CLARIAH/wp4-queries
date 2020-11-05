f9440b3e9c5ce6abdb6556ed28865759ee4efaae
RubenSchalk <rubenschalk1@gmail.com>
2020-11-05 11:52:10 +0100

    Update query_results_with_authentication.R

--- query_results_with_authentication.R
+++ query_results_with_authentication.R
@@ -1,13 +1,16 @@
 library(httr) # needed for the GET command
-# Source: https://nightly.triplydb.com/BennyMarkovitch/-/queries/Query
-key = "token" # insert token here
-data <- content(GET(("https://api.nightly.triplydb.com/queries/BennyMarkovitch/Query/run.csv?page=1&pageSize=10000"),  add_headers(Authorization = paste("bearer", key)))) # this link stores the query's output
+library(jsonlite) # needed for fromJSON
+key = "token"
+data <- GET(("https://api.druid.datalegend.net/queries/dataLegend/microheights-to-table/15/run?page=1&pageSize=10000"),  add_headers(Authorization = paste("bearer", key)))
+data <- fromJSON(content(data, as = 'text', encoding = "UTF-8"))
 page = 1 # counts how many pages you queried so far
 cont = nrow(data) == 10000 # tells the loop whether to start
-while(cont && page < 100){ # prevents an endless loop; adjust the maximum number of pages based on your dataset's size.
+while(cont && page < 500){ # prevents an endless loop; adjust the maximum number of pages based on your dataset's size.
   page = page+1
   n1 = nrow(data) # how many rows existed before
-  data <- rbind(data, content(GET(sprintf("https://api.nightly.triplydb.com/queries/BennyMarkovitch/Query/run.csv?page=%i&pageSize=10000", page),  add_headers(Authorization = paste("bearer", key))))) # queries the next page
+  fetch <- GET(sprintf("https://api.druid.datalegend.net/queries/dataLegend/microheights-to-table/15/run?page=%i&pageSize=10000", page),  add_headers(Authorization = paste("bearer", key))) # queries the next page
+  fetch <- fromJSON(content(fetch, as = 'text', encoding = "UTF-8"))
+  data = rbind(data, fetch)
   n2 = nrow(data) # how many rows exist after
   cont = (n2 - n1) == 10000 # checks whether the next query will pick any more rows; if not, the loop stops
   #print(page) # if you want to follow what's going on
